3de7a221ff22a530b0b663a28037ed99c799c77d
gabeno <gmajivu@gmail.com>
2014-01-24 12:37:57 +0300

    best practices: you should avoid global variables

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,7 +8,11 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     globals : function() {
-      myObject = {
+      // LESSON: always use var keyword
+      // myObject = {
+      //   name : 'Jory'
+      // };
+      var myObject = {
         name : 'Jory'
       };
 

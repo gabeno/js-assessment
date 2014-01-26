5804b19627b7922ffb935603a5727c8d1d5cd861
gabeno <gmajivu@gmail.com>
2014-01-26 18:00:25 +0300

    objects and context: you should be able to alter the context in which a method runs

--- app/objects.js
+++ app/objects.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.bind(obj)(); // decorate obj w/ fn and call it
     },
 
     alterObjects : function(constructor, greeting) {

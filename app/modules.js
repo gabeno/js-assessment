c2ed7f4a13627976d83d4ff5c0ad77df92446e8c
gabeno <gmajivu@gmail.com>
2014-01-27 10:36:26 +0300

    module pattern: you should be able to create a function that returns a module

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,17 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
+      return (function() {
+        return {
+          sayIt: function() {
+            return this.greeting + ', ' + this.name;
+          },
 
+          name: str2,
+
+          greeting: str1
+        };
+      }());
     }
   };
 });

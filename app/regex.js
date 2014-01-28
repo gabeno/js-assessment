b51451be1e7c893bfdcfab845e7fe819923e8282
gabeno <gmajivu@gmail.com>
2014-01-28 12:32:49 +0300

    regular expressions: you should be able to detect a number in a string

--- app/regex.js
+++ app/regex.js
@@ -3,7 +3,8 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        var pattern = /(\d+)/;
+        return pattern.test(str);
     },
 
     containsRepeatingLetter : function(str) {

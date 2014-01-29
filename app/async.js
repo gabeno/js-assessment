e2d5963308650799d83d325b2815c990597f3cfd
gabeno <gmajivu@gmail.com>
2014-01-29 14:29:28 +0300

    ascync behaviour: you should understand how to use promises to handle asynchronicity

--- app/async.js
+++ app/async.js
@@ -3,7 +3,8 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      var dfd = $.Deferred();
+      return dfd.resolve(value);
     },
 
     manipulateRemoteData : function(url) {

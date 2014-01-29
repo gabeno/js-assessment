ea90f73e96b2e2cf2ddcb0e79f2fb185f5f3dcf8
gabeno <gmajivu@gmail.com>
2014-01-29 13:39:41 +0300

    numbers::binary operations - using bitwise operator & niceness

--- app/numbers.js
+++ app/numbers.js
@@ -11,12 +11,19 @@ define(function() {
     },
 
     convertToBinary: function(num) {
-        var bin = num.toString(2);
-        // pad to an 8-bit string
-        if (bin.length < 8) {
-            bin = '00000000'.split('').slice(0, 8 - bin.length).join('') + bin;
-        }
-        return bin;
+        // var bin = num.toString(2);
+        // // pad to an 8-bit string
+        // if (bin.length < 8) {
+        //     bin = '00000000'.split('').slice(0, 8 - bin.length).join('') + bin;
+        // }
+        // return bin;
+
+        var index, bitMask, bin = [];
+
+        for(bitMask = 128, index = 0; bitMask > 0; bitMask >>= 1, index++)
+            bin.push((bitMask & num) ? 1 : 0)
+
+        return bin.join('');
     },
 
     multiply: function(a, b) {

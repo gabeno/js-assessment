13050ebc48fead866fa59f322f0a82c3f4dfd54c
gabeno <gmajivu@gmail.com>
2014-01-29 14:01:50 +0300

    numbers::binary operations - you should be able to find the value of a given bit

--- app/numbers.js
+++ app/numbers.js
@@ -3,7 +3,9 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+        var num = this.convertToBinary(num);
+        var len = num.length;
+        return +num.charAt(len - bit);
     },
 
     base10: function(str) {

053abbbbe80663f6c964bef97d029e62d03fc09d
gabeno <gmajivu@gmail.com>
2014-01-30 11:54:19 +0300

    counter: count once per 1/10th of a second, cancel the counting

--- app/count.js
+++ app/count.js
@@ -3,6 +3,20 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      function Counter() {
+        if (start <= end) {
+          console.log(start);
+        }
+        start++;
+        
+        this.timerId = setTimeout(Counter, 100);
+      }
+
+      Counter.prototype.cancel = function() {
+        clearTimeout(this.timerId);
+      };
+      
+      return new Counter();
     }
   };
 });
